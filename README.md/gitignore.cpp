ceffe0f7cebf76f157bc5af13fbc3987764a3cc0
Mnisi-WSU <mnisithembisa41@gmail.com>
2026-09-01 18:02:22 +0200

    Create .gitignore for C++ build and IDE files
    Add a .gitignore file for C++ project build artifacts and IDE settings.

---
+++ README.md/gitignore.cpp
@@ -0,0 +1,26 @@
+# Compiled files
+*.exe
+*.o
+*.obj
+*.out
+
+# Build directories
+build/
+bin/
+obj/
+
+# CMake
+CMakeCache.txt
+CMakeFiles/
+cmake_install.cmake
+Makefile
+
+# Code::Blocks
+*.depend
+*.layout
+
+# Visual Studio Code
+.vscode/
+
+# Visual Studio
+.vs/
